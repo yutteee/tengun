d1dda37916bc2da1a6ceb436cf8103043f61e54b
yutteee <yusaku_nakamura_aa@icloud.com>
2022-05-08 22:39:45 +0900

    Cnvページの間違いを訂正

--- src/views/CyclicVertical.vue
+++ src/views/CyclicVertical.vue
@@ -14,7 +14,7 @@ export default {
     data() {
         return{
             PhotoUrl: require('../assets/C3v.png'),
-            AnswerName: 'C3v'
+            AnswerName: 'Cnv'
         }
     }
 }
